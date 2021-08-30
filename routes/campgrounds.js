7fd2a512df9657c138098e07d9a1e7373738e506
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-08-30 22:28:53 +0200

    Add more error handling and messages
    when using an invalid campground id in the url, etc...

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -42,12 +42,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 //for displaying details of a chosen campground
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'That campground does not exist');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/details.ejs', { campground });
 }));
 
 //route for edit form
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'That campground does not exist');
+        res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit.ejs', { campground });
 }));
 
