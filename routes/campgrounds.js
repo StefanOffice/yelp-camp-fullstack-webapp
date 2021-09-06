7f903b35678debac5c80939668a0c47d8e810e57
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-09-06 14:46:58 +0200

    Restrict access to certain routes based on login status

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const {campgroundSchema} = require('../schemas.js');
+const {isLoggedIn} = require('../middleware')
 
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
@@ -27,12 +28,12 @@ router.get('/', catchAsync(async (req, res) => {
 }));
 
 //must be before 'campgrounds/:id' route otherwise it will see 'new' as something that goes to ':id'
-router.get('/new', (req, res) => {
-    res.render('campgrounds/new.ejs');
+router.get('/new', isLoggedIn, (req, res) => {
+        res.render('campgrounds/new.ejs');
 });
 
 //second part of the creating a new campground process, form from new.ejs will hit this route
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const newCamp = new Campground(req.body.campground);
     await newCamp.save();
     req.flash('success', 'Successfully made a new campground!');
@@ -50,7 +51,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 //route for edit form
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if(!campground){
         req.flash('error', 'That campground does not exist');
@@ -60,7 +61,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 }));
 
 //route that edit form sends data to
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     //data is grouped under 'campground' so we can just use spread
     const updatedCamp = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
@@ -70,7 +71,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 
 
 //route for deleting a campground
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Campground deleted!');
