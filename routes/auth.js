c9311abe94d89819e19d989a5d10f6abdc08d146
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-09-06 13:58:22 +0200

    Add user registration logic
    users can now register through the form and are saved to the database

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 })
+
+router.post('/register', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const newUser = new User({ email, username });
+        const regUser = await User.register(newUser, password);
+        req.flash('success', `Hello ${username}, you've been registered successfully!`);
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+});
 
 module.exports = router;
