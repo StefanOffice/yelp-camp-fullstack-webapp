2a035530ae59f5dbc92b60815323167325a00672
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-09-06 14:22:08 +0200

    Add login form and routes
    username and pass are checked in the database
    but login is not persisted

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
@@ -19,5 +20,20 @@ router.post('/register', async (req, res) => {
         res.redirect('register');
     }
 });
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+const passOptions = {
+    failureFlash: true,
+    failureRedirect : '/login'
+}
+
+router.post('/login', passport.authenticate('local', passOptions), (req, res) => {
+
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
